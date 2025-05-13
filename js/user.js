61bd203e6711f83277f54e604665106b3636c128
Japp666 <4lucifer@gmail.com>
2025-05-13 11:50:02 +0300

    Update user.js

--- js/user.js
+++ js/user.js
@@ -1,1 +1,23 @@
+// user.js
+
+export const user = {
+  name: '',
+  race: '',
+  resources: {
+    metal: 1000,
+    crystal: 800,
+    energy: 500
+  },
+  score: 0
+};
+
+let isBuildingInProgress = false;
+
+export function getBuildingStatus() {
+  return isBuildingInProgress;
+}
+
+export function setBuildingStatus(status) {
+  isBuildingInProgress = status;
+}
 
