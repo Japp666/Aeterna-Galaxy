4945fab225c200a83091caba8bbe2a8846edf40d
Japp666 <4lucifer@gmail.com>
2025-05-19 16:12:03 +0300

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,34 +1,35 @@
+import { user } from './user.js';
+
 export function initMap() {
-  const map = document.getElementById('mapGrid');
-  map.innerHTML = '';
+    const map = document.getElementById('map');
+    map.innerHTML = '';
+    const width = 10;
+    const height = 10;
 
-  const width = 10;
-  const height = 10;
+    for (let y = 0; y < height; y++) {
+        const row = document.createElement('div');
+        row.className = 'map-row';
 
-  for (let y = 0; y < height; y++) {
-    const row = document.createElement('div');
-    row.className = 'map-row';
+        for (let x = 0; x < width; x++) {
+            const cell = document.createElement('div');
+            cell.className = 'map-cell';
+            cell.dataset.coords = `${x},${y}`;
 
-    for (let x = 0; x < width; x++) {
-      const cell = document.createElement('div');
-      cell.className = 'map-cell';
-      cell.dataset.coords = `${x},${y}`;
+            if (x === 5 && y === 5) {
+                cell.classList.add('player-position');
+                cell.innerHTML = `<span class="map-player">👨‍🚀</span>`;
+                cell.title = `Comandant ${user.name}\nCoordonate: [${x}:${y}]\nPuncte: ${user.score}`;
+            }
 
-      if (x === 5 && y === 5) {
-        cell.classList.add('player-position');
-        cell.innerHTML = '<span class="map-player">👨‍🚀</span>';
-        cell.title = `Comandant ${window.user.name}\nCoordonate: [${x}:${y}]\nPuncte: ${window.user.score}`;
-      }
+            if (x === 7 && y === 3) {
+                cell.classList.add('bot-position');
+                cell.innerHTML = `<span class="map-player bot">🤖</span>`;
+                cell.title = `Comandant Zeta (BOT)\nCoordonate: [${x}:${y}]\nPuncte: 1340`;
+            }
 
-      if (x === 7 && y === 3) {
-        cell.classList.add('bot-position');
-        cell.innerHTML = '<span class="map-player bot">🤖</span>';
-        cell.title = `Comandant Zeta (BOT)\nCoordonate: [${x}:${y}]\nPuncte: 1340`;
-      }
+            row.appendChild(cell);
+        }
 
-      row.appendChild(cell);
+        map.appendChild(row);
     }
-
-    map.appendChild(row);
-  }
 }
