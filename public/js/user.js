aa78dc235823327703ef0de99a717782559685f5
Japp666 <4lucifer@gmail.com>
2025-05-19 16:01:39 +0300

    Update user.js

--- public/js/user.js
+++ public/js/user.js
@@ -1,13 +1,45 @@
 export const user = {
-  name: '',
-  race: '',
-  score: 0,
-  lastOnline: Date.now(),
-  buildings: {},
-  research: {},
-  resources: {
-    metal: 1000,
-    crystal: 1000,
-    energy: 1000
-  }
+    name: '',
+    race: '',
+    resources: {
+        metal: 1000,
+        crystal: 800,
+        energy: 500
+    },
+    score: 0,
+    buildings: {},
+    research: {},
+    fleet: {
+        small: 0,
+        medium: 0,
+        large: 0
+    },
+    lastOnline: Date.now()
 };
+
+// Funcții pentru gestionarea resurselor și progresului jucătorului
+export function updateResources(type, amount) {
+    if (user.resources[type] !== undefined) {
+        user.resources[type] += amount;
+    }
+}
+
+export function deductResources(cost) {
+    Object.keys(cost).forEach(resource => {
+        if (user.resources[resource] >= cost[resource]) {
+            user.resources[resource] -= cost[resource];
+        }
+    });
+}
+
+export function canAfford(cost) {
+    return Object.keys(cost).every(resource => user.resources[resource] >= cost[resource]);
+}
+
+export function updateScore(points) {
+    user.score += points;
+}
+
+export function getUserData() {
+    return JSON.parse(JSON.stringify(user)); // Returnează o copie sigură a obiectului
+}
