cb5df612da4131313bd14100a9c3d72132cebd2f
Japp666 <4lucifer@gmail.com>
2025-05-20 10:23:55 +0300

    Update menu.js

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,1 +1,6 @@
+import { showBuildings } from './buildings.js';
+import { showResearch } from './research.js';
+import { initMap } from './map.js';
+import { showFleet } from './fleet.js';
 
+export
