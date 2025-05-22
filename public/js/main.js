02c47f27c9539f7cf5916038db56c14d8f955153
Japp666 <4lucifer@gmail.com>
2025-05-22 15:17:23 +0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -4,10 +4,9 @@ import { getPlayerName, getPlayerRace, loadPlayerData } from './user.js';
 import { updateHUD } from './hud.js';
 import { loadTabContent } from './menu.js';
 import { initBotAI } from './bot.js';
-import { auth } from './firebase-config.js';
 import { showLoginModal } from './login.js';
 
-auth.onAuthStateChanged(async (user) => {
+firebase.auth().onAuthStateChanged(async (user) => {
     if (user) {
         console.log(`Utilizator autentificat: ${user.email}`);
         await loadPlayerData();
