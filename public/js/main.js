90c69f2415dde6cdefc5de3dd44028ae266b6f30
Japp666 <4lucifer@gmail.com>
2025-05-22 15:49:03 +0300

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,24 @@
-// public/js/main.js
-import { showNameModal, showRaceSelectionScreen, showMessage } from './utils.js';
+import { showRaceSelectionScreen, showMessage } from './utils.js';
 import { getPlayerName, getPlayerRace, loadPlayerData } from './user.js';
 import { updateHUD } from './hud.js';
 import { loadTabContent } from './menu.js';
 import { initBotAI } from './bot.js';
-import { showLoginModal } from './login.js';
+import { showNicknameModal } from './login.js';
 
-firebase.auth().onAuthStateChanged(async (user) => {
-    if (user) {
-        console.log(`Utilizator autentificat: ${user.email}`);
-        await loadPlayerData();
-        const playerName = getPlayerName();
-        const playerRace = getPlayerRace();
+async function initializeGame() {
+    await showNicknameModal();
+    const playerName = getPlayerName();
+    const playerRace = getPlayerRace();
 
-        if (!playerName) {
-            await showNameModal();
-            updateHUD();
-        }
-        if (!playerRace) {
-            await showRaceSelectionScreen();
-            updateHUD();
-        }
-
-        await loadTabContent('hud', 'hud');
-        loadTabContent('home');
-        initBotAI();
-        showMessage(`Jocul a pornit! Bun venit, ${playerName} al rasei ${playerRace}!`, 'success');
-    } else {
-        await showLoginModal();
+    if (!playerRace) {
+        await showRaceSelectionScreen();
+        updateHUD();
     }
-});
+
+    await loadTabContent('hud', 'hud');
+    loadTabContent('home');
+    initBotAI();
+    showMessage(`Jocul a pornit! Bun venit, ${playerName} al rasei ${playerRace || 'necunoscute'}!`, 'success');
+}
+
+initializeGame();
