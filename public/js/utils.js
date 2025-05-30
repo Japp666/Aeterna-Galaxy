43f1ff9d5727c7c96251697813335f1de5f1d653
Japp666 <4lucifer@gmail.com>
2025-05-30 14:01:08 +0300

    Update utils.js

--- public/js/utils.js
+++ public/js/utils.js
@@ -6,14 +6,14 @@ const gameState = {
     production: { metal: 0, crystal: 0, helium: 0, energy: 0, research: 0 },
     buildings: {},
     buildingsList: [
-        { key: 'metal_mine', name: 'Mina de Metal', baseCost: { metal: 60, crystal: 15 }, baseBuildTime: 10, production: { metal: 50 }, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' },
-        { key: 'crystal_mine', name: 'Mina de Cristal', baseCost: { metal: 48, crystal: 24 }, baseBuildTime: 15, production: { crystal: 30 }, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' },
-        { key: 'helium_refinery', name: 'Rafinărie de Heliu', baseCost: { metal: 100, crystal: 40, helium: 20 }, baseBuildTime: 20, production: { helium: 20 }, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' },
-        { key: 'solar_plant', name: 'Centrală Solară', baseCost: { metal: 75, crystal: 30 }, baseBuildTime: 12, production: { energy: 40 }, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' },
-        { key: 'shipyard', name: 'Șantier Naval', baseCost: { metal: 200, crystal: 100, helium: 50 }, baseBuildTime: 30, production: {}, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' },
-        { key: 'research_lab', name: 'Laborator de Cercetare', baseCost: { metal: 150, crystal: 80, helium: 30 }, baseBuildTime: 25, production: { research: 10 }, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' },
-        { key: 'defense_turret', name: 'Turn de Apărare', baseCost: { metal: 120, crystal: 50 }, baseBuildTime: 20, production: {}, hp: 100, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' },
-        { key: 'orbital_station', name: 'Stație Orbitală', baseCost: { metal: 300, crystal: 150 }, baseBuildTime: 40, production: {}, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' }
+        { key: 'metal_mine', name: 'Mina de Metal', baseCost: { metal: 600, crystal: 150 }, baseBuildTime: 10, production: { metal: 500 }, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' },
+        { key: 'crystal_mine', name: 'Mina de Cristal', baseCost: { metal: 480, crystal: 240 }, baseBuildTime: 15, production: { crystal: 300 }, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' },
+        { key: 'helium_refinery', name: 'Rafinărie de Heliu', baseCost: { metal: 1000, crystal: 400, helium: 200 }, baseBuildTime: 20, production: { helium: 200 }, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' },
+        { key: 'solar_plant', name: 'Centrală Solară', baseCost: { metal: 750, crystal: 300 }, baseBuildTime: 12, production: { energy: 400 }, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' },
+        { key: 'shipyard', name: 'Șantier Naval', baseCost: { metal: 2000, crystal: 1000, helium: 500 }, baseBuildTime: 30, production: {}, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' },
+        { key: 'research_lab', name: 'Laborator de Cercetare', baseCost: { metal: 1500, crystal: 800, helium: 300 }, baseBuildTime: 25, production: { research: 100 }, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' },
+        { key: 'defense_turret', name: 'Turn de Apărare', baseCost: { metal: 1200, crystal: 500 }, baseBuildTime: 20, production: {}, hp: 100, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' },
+        { key: 'orbital_station', name: 'Stație Orbitală', baseCost: { metal: 3000, crystal: 1500 }, baseBuildTime: 40, production: {}, image: 'https://i.postimg.cc/ydLx2C1L/coming-soon.png' }
     ],
     researches: {},
     researchesList: [
@@ -26,14 +26,15 @@ const gameState = {
     ],
     fleet: [],
     fleetList: [
-        { key: 'hunter', name: 'Vânător', cost: { metal: 100, crystal: 50, helium: 20 }, time: 20, attack: 50, hp: 100, speed: 10 },
-        { key: 'cruiser', name: 'Crucișător', cost: { metal: 200, crystal: 100, helium: 50 }, time: 30, attack: 100, hp: 200, speed: 8 },
-        { key: 'dreadnought', name: 'Dreadnought', cost: { metal: 400, crystal: 200, helium: 100 }, time: 50, attack: 200, hp: 500, speed: 5 }
+        { key: 'hunter', name: 'Vânător', cost: { metal: 1000, crystal: 500, helium: 200 }, time: 20, attack: 50, hp: 100, speed: 10 },
+        { key: 'cruiser', name: 'Crucișător', cost: { metal: 2000, crystal: 1000, helium: 500 }, time: 30, attack: 100, hp: 200, speed: 8 },
+        { key: 'dreadnought', name: 'Dreadnought', cost: { metal: 4000, crystal: 2000, helium: 1000 }, time: 50, attack: 200, hp: 500, speed: 5 }
     ],
     isBuilding: false,
     isResearching: false,
     isBuildingShip: false,
-    raceBonus: {}
+    raceBonus: {},
+    players: [] // Adăugat pentru harta multiplayer
 };
 
 async function loadComponent(component, targetId = 'content') {
@@ -101,7 +102,7 @@ function updateResources() {
         const production = gameState.production[resource] || 0;
         const bonus = gameState.raceBonus[resource] || 1;
         const newValue = gameState.resources[resource] + (production * bonus) / 60; // 1 minut
-        const max = resource === 'research' ? Infinity : { metal: 10000, crystal: 10000, helium: 5000, energy: 5000 }[resource];
+        const max = resource === 'research' ? Infinity : { metal: 100000, crystal: 100000, helium: 50000, energy: 50000 }[resource];
         if (Math.abs(newValue - gameState.resources[resource]) > 0.1) {
             gameState.resources[resource] = Math.min(newValue, max);
             hasChanged = true;
