4f01099bb99769eb091af76835767f8e99d8bb8b
Japp666 <4lucifer@gmail.com>
2025-06-23 10:42:05 +0300

    Update overviewCard.js

--- public/assets/js/components/overviewCard.js
+++ public/assets/js/components/overviewCard.js
@@ -1,1 +1,10 @@
+import { el } from "../utils/dom.js";
 
+export default {
+  create({ title, value }) {
+    return el("div", { class: "overview-card" },
+      el("h3", {}, title),
+      el("div", { class: "overview-value" }, value)
+    );
+  }
+};
