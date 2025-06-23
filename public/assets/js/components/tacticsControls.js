ac8d99fa3b23267698ee3b2f6c564d21933633f4
Japp666 <4lucifer@gmail.com>
2025-06-23 10:42:49 +0300

    Update tacticsControls.js

--- public/assets/js/components/tacticsControls.js
+++ public/assets/js/components/tacticsControls.js
@@ -1,1 +1,16 @@
+import { el } from "../utils/dom.js";
 
+export default {
+  create() {
+    return el("div", { class: "tactics-controls" },
+      el("button", { onclick: () => console.log("Pressing ↑") }, "Pressing ↑"),
+      el("button", { onclick: () => console.log("Defensive Line ↓") }, "Defensive Line ↓"),
+      el("button", { onclick: () => console.log("Tempo ↑") }, "Tempo ↑"),
+      el("button", { onclick: () => console.log("Tempo ↓") }, "Tempo ↓"),
+      el("button", { onclick: () => console.log("Toggle Auto-Subs") }, "Toggle Auto-Subs")
+    );
+  },
+  onEnd(matchData) {
+    console.log("Match ended:", matchData);
+  }
+};
