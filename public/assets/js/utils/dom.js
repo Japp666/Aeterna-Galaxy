0bb0cca78e57d46342d219232115250c4e8c6fd6
Japp666 <4lucifer@gmail.com>
2025-06-23 10:29:52 +0300

    Update dom.js

--- public/assets/js/utils/dom.js
+++ public/assets/js/utils/dom.js
@@ -1,8 +1,11 @@
+// public/assets/js/utils/dom.js
 export function el(tag, attrs = {}, ...children) {
-  const e = document.createElement(tag);
-  Object.entries(attrs).forEach(([key, value]) => e.setAttribute(key, value));
-  children.forEach(child => {
-    e.append(typeof child === "string" ? document.createTextNode(child) : child);
+  const element = document.createElement(tag);
+  Object.entries(attrs).forEach(([key, value]) => {
+    element.setAttribute(key, value);
   });
-  return e;
+  children.forEach(child => {
+    element.append(typeof child === "string" ? document.createTextNode(child) : child);
+  });
+  return element;
 }
