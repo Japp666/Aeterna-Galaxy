39bfa9ea57b63285ac561a555ce49c0135b5bdc8
Japp666 <4lucifer@gmail.com>
2025-06-23 16:30:04 +0300

    Update dom.js

--- public/assets/js/utils/dom.js
+++ public/assets/js/utils/dom.js
@@ -1,8 +1,8 @@
 export function el(tag, attrs = {}, ...children) {
   const element = document.createElement(tag);
-  for (const [key, value] of Object.entries(attrs)) {
+  Object.entries(attrs).forEach(([key, value]) => {
     element.setAttribute(key, value);
-  }
+  });
   children.forEach(child => {
     element.append(typeof child === "string" ? document.createTextNode(child) : child);
   });
