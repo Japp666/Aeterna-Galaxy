7822ead1ac8832eadf8c4b115963dbd3b77d05d6
Japp666 <4lucifer@gmail.com>
2025-06-23 10:38:58 +0300

    Update index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,11 +1,10 @@
-// public/assets/js/index.js
 import { navigateTo } from "./router.js";
 import NavBar from "./components/navBar.js";
 
-// Inițializează NavBar-ul din div-ul cu id="navbar"
+// Inițializează meniul de navigare
 NavBar.init(navigateTo);
 
-// La încărcarea completă a paginii, navigăm către dashboard
+// La încărcarea documentului, navigăm către pagina "dashboard"
 document.addEventListener("DOMContentLoaded", () => {
   navigateTo("dashboard");
 });
