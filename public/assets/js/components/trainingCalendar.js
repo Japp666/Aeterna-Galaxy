5ee1d4230081c552b54bec6d6d13f69cce968baa
Japp666 <4lucifer@gmail.com>
2025-06-23 10:44:50 +0300

    Update trainingCalendar.js

--- public/assets/js/components/trainingCalendar.js
+++ public/assets/js/components/trainingCalendar.js
@@ -1,1 +1,9 @@
+import { el } from "../utils/dom.js";
 
+export default {
+  create() {
+    return el("div", { class: "training-calendar" },
+      el("p", {}, "Calendar de antrenamente va fi aici")
+    );
+  }
+};
