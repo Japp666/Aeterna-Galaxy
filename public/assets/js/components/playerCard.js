88e00cb1e90733c4294d9e09edc129673c959f6f
Japp666 <4lucifer@gmail.com>
2025-06-23 10:44:19 +0300

    Update playerCard.js

--- public/assets/js/components/playerCard.js
+++ public/assets/js/components/playerCard.js
@@ -1,1 +1,14 @@
+import { el } from "../utils/dom.js";
+import { fmtEuro } from "../utils/formatter.js";
 
+export default {
+  create(player) {
+    return el("div", { class: "player-card" },
+      el("h4", {}, player.name),
+      el("p", {}, `Vârstă: ${player.age}`),
+      el("p", {}, `Potențial: ${player.potential}`),
+      el("p", {}, `Salariu: ${fmtEuro(player.salary)}`),
+      el("p", {}, `Valoare piață: ${fmtEuro(player.marketValue)}`)
+    );
+  }
+};
