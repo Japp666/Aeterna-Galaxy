403d4cb46a56fa621867a00d14f7eee687cb5691
Japp666 <4lucifer@gmail.com>
2025-06-23 09:59:30 +0300

    Update index.js

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,1 +1,10 @@
+import { navigateTo } from "./router.js";
+import NavBar from "./components/navBar.js";
 
+// Initializează NavBar-ul (se injectează în div-ul cu id="navbar")
+NavBar.init(navigateTo);
+
+// La load-ul paginii, navigăm către dashboard
+document.addEventListener("DOMContentLoaded", () => {
+  navigateTo("dashboard");
+});
