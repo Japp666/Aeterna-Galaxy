29ab05765f51109995b1f4f2e3e7207272fa0965
Japp666 <4lucifer@gmail.com>
2025-06-23 10:39:44 +0300

    Update formatter.js

--- public/assets/js/utils/formatter.js
+++ public/assets/js/utils/formatter.js
@@ -1,1 +1,7 @@
+export function fmtEuro(value) {
+  return new Intl.NumberFormat("ro-RO", { style: "currency", currency: "EUR" }).format(value);
+}
 
+export function fmtDate(timestamp) {
+  return new Date(timestamp).toLocaleString("ro-RO");
+}
