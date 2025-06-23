b11822215a8b4e2e8fb6d70401a5f024df72e7f2
Japp666 <4lucifer@gmail.com>
2025-06-23 10:43:32 +0300

    Update heatmap.js

--- public/assets/js/components/heatmap.js
+++ public/assets/js/components/heatmap.js
@@ -1,1 +1,9 @@
+import { el } from "../utils/dom.js";
 
+export default {
+  create() {
+    return el("div", { class: "heatmap" },
+      el("p", {}, "Heatmap (minimap) goes here")
+    );
+  }
+};
