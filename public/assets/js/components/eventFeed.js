feb818f2c912e0141fe17ee1c2268aac28478cbe
Japp666 <4lucifer@gmail.com>
2025-06-23 10:42:35 +0300

    Update eventFeed.js

--- public/assets/js/components/eventFeed.js
+++ public/assets/js/components/eventFeed.js
@@ -1,1 +1,13 @@
+import { el } from "../utils/dom.js";
 
+export default {
+  create() {
+    return el("div", { class: "event-feed" },
+      el("p", {}, "Evenimentele vor apărea aici...")
+    );
+  },
+  onUpdate(event) {
+    console.log("Update event:", event);
+    // Actualizează UI-ul (ex.: adaugă evenimente în feed)
+  }
+};
