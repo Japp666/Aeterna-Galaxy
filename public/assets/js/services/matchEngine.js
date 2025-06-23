aaa561cc1ff607d0afa621b4f67591398f864237
Japp666 <4lucifer@gmail.com>
2025-06-23 10:02:21 +0300

    Update matchEngine.js

--- public/assets/js/services/matchEngine.js
+++ public/assets/js/services/matchEngine.js
@@ -1,1 +1,22 @@
+let tickInterval;
 
+export function simulateMatch(callbacks) {
+  const totalTicks = 90;
+  let tick = 0;
+  tickInterval = setInterval(() => {
+    tick++;
+    if (callbacks && callbacks.onUpdate) {
+      callbacks.onUpdate({ minute: tick, events: [] });
+    }
+    if (tick >= totalTicks) {
+      clearInterval(tickInterval);
+      if (callbacks && callbacks.onEnd) {
+        callbacks.onEnd({ finalScore: { home: 1, away: 2 }, events: [] });
+      }
+    }
+  }, 20 * 1000); // 20 secunde pentru fiecare "tick"
+}
+
+export function cancelMatch() {
+  clearInterval(tickInterval);
+}
