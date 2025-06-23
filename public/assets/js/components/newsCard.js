b485a12e2a383f18a7bf81d2ff109a4e9e89d60a
Japp666 <4lucifer@gmail.com>
2025-06-23 10:42:20 +0300

    Update newsCard.js

--- public/assets/js/components/newsCard.js
+++ public/assets/js/components/newsCard.js
@@ -1,1 +1,14 @@
+import { el } from "../utils/dom.js";
 
+export default {
+  create({ icon, title, subtitle, isNew = false }) {
+    return el("div", { class: "news-card" },
+      el("span", { class: "news-icon" }, icon),
+      el("div", { class: "news-content" },
+        el("h4", {}, title),
+        el("small", {}, subtitle)
+      ),
+      isNew ? el("span", { class: "news-badge" }, "New") : ""
+    );
+  }
+};
