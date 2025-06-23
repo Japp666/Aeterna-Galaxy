f1029b1c7e92313f8ce2b57aba37d917ff4e018c
Japp666 <4lucifer@gmail.com>
2025-06-23 10:41:51 +0300

    Update header.js

--- public/assets/js/components/header.js
+++ public/assets/js/components/header.js
@@ -1,1 +1,10 @@
+import { el } from "../utils/dom.js";
 
+export default {
+  create(title) {
+    return el("header", { class: "header" },
+      el("h1", {}, title),
+      el("button", { onclick: () => alert("Settings") }, "⚙")
+    );
+  }
+};
