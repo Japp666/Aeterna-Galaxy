ee975187d1b296348721f7f939a0baea8f3ffc6d
Japp666 <4lucifer@gmail.com>
2025-06-23 10:44:35 +0300

    Update offerModal.js

--- public/assets/js/components/offerModal.js
+++ public/assets/js/components/offerModal.js
@@ -1,1 +1,17 @@
+import { el } from "../utils/dom.js";
 
+export default {
+  create(player) {
+    const modal = el("div", { class: "offer-modal" },
+      el("h3", {}, `Oferă pentru ${player.name}`),
+      el("label", {}, "Salariu: "),
+      el("input", { type: "number", value: player.salary }),
+      el("label", {}, "Taxă Transfer: "),
+      el("input", { type: "number", value: player.marketValue }),
+      el("button", { onclick: () => console.log("Offer sent!") }, "Trimite ofertă"),
+      el("button", { onclick: () => modal.remove() }, "Anulează")
+    );
+    document.body.appendChild(modal);
+    return modal;
+  }
+};
