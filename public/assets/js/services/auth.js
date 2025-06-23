9b7ce7fe1fbb92828330359ad6f3cf479968c8ff
Japp666 <4lucifer@gmail.com>
2025-06-23 10:40:10 +0300

    Update auth.js

--- public/assets/js/services/auth.js
+++ public/assets/js/services/auth.js
@@ -1,1 +1,15 @@
+// Stub pentru autentificare (Firebase Auth sau alt sistem)
+export async function login(email, password) {
+  // TODO: Implementare login
+  return { userId: '123', email };
+}
 
+export async function signup(email, password) {
+  // TODO: Implementare signup
+  return { userId: '123', email };
+}
+
+export function onAuthStateChanged(callback) {
+  // Simulare: userul este null la început
+  callback(null);
+}
