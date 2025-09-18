5ee18494243e079c6f1930eb9e39481aba034294
Japp666 <4lucifer@gmail.com>
2025-09-18 15:58:06 +0300

    Delete public/js directory

--- public/js/main.js
+++
@@ -1,7 +0,0 @@
-// main.js
-import { initializeGameUI } from './game-ui.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("main.js: DOM a fost încărcat. Inițializare UI joc...");
-    initializeGameUI();
-});
